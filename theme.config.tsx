9b16cbca64c037980b41a104b9caf0ddcecd9790
Michal Dzianach <michaldzianach@iMac.local>
2023-03-31 13:42:57 +0100

    <major info update>

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React  from 'react'
+import { DocsThemeConfig, useNextSeoProps } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Unick.io Documentation</span>,
+  
+  logo: (
+      <span style={{ fontWeight: 900 }}>
+        Unick Docs
+      </span>
+  ),
   project: {
-    link: 'https://unick.io',
+    link: 'https://github.com/Unick-io/Documentation',
   },
   chat: {
     link: 'https://discord.gg/gpnzJXBG8m',
   },
-  docsRepositoryBase: 'https://github.com/Unick-io/Documentation',
+  docsRepositoryBase: 'https://github.com/Unick-io',
   footer: {
     text: 'Unick.io Docs',
   },
-};
+  useNextSeoProps() {
+    return {
+
+  titleTemplate: '%s – Unick.io',
+}
+  }}
+
 
 export default config
