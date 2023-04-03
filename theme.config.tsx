4e34b16b73d6bb3f782ed0b73037db4bbfb1e945
Michal Dzianach <michaldzianach@iMac.local>
2023-04-03 13:30:34 +0100

    config update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 
-const config: { DocsThemeConfig: any, useNextSeoProps: any } = {
+const config: DocsThemeConfig = {
 
   logo: (
     <span style={{ fontWeight: 900 }}>
